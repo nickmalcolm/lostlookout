866c21dd6d55a27a01ecc51220e2e1703ffc4c89
Nick Malcolm <nick@southgatelabs.com>
2011-03-11 21:45:48 +1300

    Tidies up and caches javascript and css (using rails cache)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 	$(".nav li").click(function(){
 		window.location = ($(this).find("a").attr("href"));
 	});
-	
+  $('.watermarked_title').watermark('watermark');
+});
+$(function(){
+  $(".tooltip").tipTip();
 });
