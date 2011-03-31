790faa57c03d178e132fd9c784a429b98f9964ac
Nick Malcolm <nick@southgatelabs.com>
2011-03-31 23:14:21 +1300

    Adds meta stuff to profiles and dashboard

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,9 @@ before_filter :authenticate_user!, :except => :show
   def dashboard
     @username = current_user.display_name
     @joindate = current_user.created_at
-    @content_for_title = @username+"'s Dashboard"
+    @content_for_title = "Your Lookout"
+    @meta_descr = @content_for_title+" on map based Lost Lookout."
+    @meta_tags = "lookout, dashboard, listings, settings"
     
     order = "title ASC"
     if params[:sort]
@@ -20,13 +22,16 @@ before_filter :authenticate_user!, :except => :show
     @username = user.display_name
     @joindate = user.created_at
     
+    @content_for_title = @username+"'s Profile"
+    @meta_descr = @content_for_title+" on map based Lost Lookout."
+    @meta_tags = @username+", profile, listings, settings"
+    
     order = "title ASC"
     if params[:sort]
       order = user.listings.sortable_to_column(params[:sort])
     end
     @listings = Listing.search "", :conditions => {:user_id => user.id}, :sort_mode => :extended, :order => order, :populate => true
     
-    @content_for_title = @username+"'s Profile"
   end
  
 end
