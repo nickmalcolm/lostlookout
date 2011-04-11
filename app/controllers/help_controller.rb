bb09540943ac99c79e53f08e571c407ea775c576
Nick Malcolm <nick@southgatelabs.com>
2011-04-11 17:44:53 +1200

    Adds meta data to help pages

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -2,13 +2,20 @@ class HelpController < ApplicationController
   
   def index
     @content_for_title = "Help"
+    @meta_descr = "Get answers to some common questions and learn how to make the most of Lost Lookout"
+    @meta_tags = "help, question, answer, faq, support"
   end
   
   def privacy
     @content_for_title = "Privacy Policy"
+    @meta_descr = "Read about Lost Lookout's commitment to your privacy"
+    @meta_tags = "security, safety, rules, conditions, privacy, data"
   end
   
   def terms
     @content_for_title = "Terms and Condition"
+    @meta_descr = "Get to know and accepts Lost Lookout's T&Cs before using the site."
+    @meta_tags = "legal, security, safety, rules, conditions, privacy, data"
   end  
+  
 end
