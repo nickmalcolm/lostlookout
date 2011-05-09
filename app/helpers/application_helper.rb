69c92f5cfbfd8ef6f5461175c6f226be5432ae60
Nick Malcolm <nick@southgatelabs.com>
2011-05-09 12:13:59 +1200

    changes facebook like url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -93,7 +93,7 @@ module ApplicationHelper
   end
   
   def facebook_home_like
-    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FLost-Lookout%2F196459227043474&amp;width=292&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=true&amp;height=290", :scrolling=>"no", :frameborder=>"0", :style=>"max-height:290px; width: 100%;overflow:hidden;", :allowtransparency=>"true", :id=>"facebook_home_like"
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Flostlookout&amp;width=292&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=true&amp;height=290", :scrolling=>"no", :frameborder=>"0", :style=>"max-height:290px; width: 100%;overflow:hidden;", :allowtransparency=>"true", :id=>"facebook_home_like"
   end
   
 end
