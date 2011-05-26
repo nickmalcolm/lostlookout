9169c1e807285128f3823e0d37ea9f19e1342da7
Nick Malcolm <nick@southgatelabs.com>
2011-05-26 22:18:30 +1200

    Adds scope and method to access devices by area

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,22 @@
 class Device < ActiveRecord::Base
   
-  validates :apid, :presence => true
-  validates :city, :presence => true
+  validates :apid, :presence => true, :uniqueness => true
+  validates :area, :presence => true
+  
+  attr_accessible :apid, :area
+  
+  scope :located_in, lambda{ |area|
+      Device.where(:area => area)
+    } 
+    
+  def self.get_APIDs_as_JSON(area)
+    devices = Device.located_in(area)
+    apids = []
+    devices.each do |d|
+      apids << d.apid
+    end
+    
+    apids
+  end
   
 end
